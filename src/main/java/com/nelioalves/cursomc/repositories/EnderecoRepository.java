9e597358fb99c492b3ce0964c64e1484fb0e62d3
Josué Cintra <josuecintra@gmail.com>
2020-02-07 15:24:52 -0300

    Inserindo um novo Cliente com POST (40)

--- src/main/java/com/nelioalves/cursomc/repositories/EnderecoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/EnderecoRepository.java
@@ -7,5 +7,13 @@ import com.nelioalves.cursomc.domain.Endereco;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+    
+    // Se você criou o projeto usando Spring Boot versão 2.x.x:
+    // https://github.com/acenelio/springboot2-ionic-backend
+
+    // No programa principal, onde na aula é mostrado:
+    //      categoriaRepository.save(Arrays.asList(cat1, cat2));
+    // Troque pelo seguinte código:
+    //      categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 
 }
