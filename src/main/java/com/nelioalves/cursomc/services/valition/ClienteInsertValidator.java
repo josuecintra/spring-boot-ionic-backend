6e81f080c50ba0e53918f34358cded344d8fb702
Josué Cintra <josuecintra@gmail.com>
2020-02-07 16:49:47 -0300

    Validação customizada: email não repetido na inserção de Cliente (43)

--- src/main/java/com/nelioalves/cursomc/services/valition/ClienteInsertValidator.java
+++ src/main/java/com/nelioalves/cursomc/services/valition/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.enuns.TipoCliente;
 import com.nelioalves.cursomc.dto.ClienteNewDTO;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
 import com.nelioalves.cursomc.resources.exceptions.FieldMessage;
 import com.nelioalves.cursomc.services.valition.utils.BR;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 	
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+
 		// Transporta a lista de erros personalizadas (com.nelioalves.cursomc.resources.exceptions.FieldMessage) para o Spring framework 
 		// Posteriormente, a lista de erros será capturada pelo framework lá no com.nelioalves.cursomc.resources.exceptions\ResourceExceptionHandler
 		for (FieldMessage e : list) {
