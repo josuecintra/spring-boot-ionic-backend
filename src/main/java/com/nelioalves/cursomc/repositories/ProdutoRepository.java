c7c3a914ae5866ecccf1068237e10062095f939e
Josué Cintra <josuecintra@gmail.com>
2020-02-12 15:02:54 -0300

    Busca de pedidos por nome e categorias (partes 1 (47) e 2 (48))

--- src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,33 @@
 package com.nelioalves.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+    // esta abordagem é uma forma de se contruir a query manualmente
+    // "4.3.4. Using @Query" https://docs.spring.io/spring-data/jpa/docs/1.8.x/reference/html/#jpa.query-methods.at-query
+    // @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+    // Page<Produto> search(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+
+    // porém o jpa faz isso automaticamente através do uso de "padrão de nomes", descritos na seção 
+    // "4.3.2. Query creation" https://docs.spring.io/spring-data/jpa/docs/1.8.x/reference/html/#jpa.query-methods.query-creation
+    
+    @Transactional(readOnly = true)
+    Page<Produto> findDistinctByNomeContainingAndCategoriasIn(
+        @Param("nome") String nome, 
+        @Param("categorias") List<Categoria> categorias, 
+        Pageable pageRequest
+    );
 }
