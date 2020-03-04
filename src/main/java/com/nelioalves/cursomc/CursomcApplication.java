2754946eb3a76cf89cd84a4ab69617deb1bb5cc7
Josué Cintra <josuecintra@gmail.com>
2020-03-04 12:37:23 -0300

    WIP Salvando primeiro arquivo no S3 (82); criação @Bean S3Service; usando properties; usou o CommandLineRunner() de CursoApplication() para testar uma classe de maneira simples e sem criar um endpoint

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,5 +1,8 @@
 package com.nelioalves.cursomc;
 
+import com.nelioalves.cursomc.services.S3Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +14,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) {
-		
+		s3Service.uploadFile("‪C:\\Temp\\thispersondoesnotexist-w1.jpg");
 	}
 
 }
